ed1f18980691162d186fe0eab8a1d9d8542e0cd5
Gianina Crespo <crespogianina98@gmail.com>
2025-05-30 18:40:07 -0300

    changes in DAO

--- src/main/java/org/example/dao/PersonaDAOImpl.java
+++ src/main/java/org/example/dao/PersonaDAOImpl.java
@@ -1,15 +1,22 @@
 package org.example.dao;
 
+import org.example.config.DatabaseConnection;
 import org.example.entities.Persona;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.List;
 
 public class PersonaDAOImpl implements GenericDAO<Persona> {
 
     @Override
     public void save(Persona persona) throws Exception {
-
-
+        String query = "INSERT INTO persona (nombre, apellido, fk_domicilio) VALUES (?, ?, ?)";
+        try (Connection conn = DatabaseConnection.getConnection(); PreparedStatement ps = conn.prepareStatement(query)) {
+            ps.setString(1, persona.getNombre());
+            ps.setInt(2, persona.getEdad());
+            ps.setInt(2, persona.getDomicilio().getNumero());
+        }
     }
 
     @Override
